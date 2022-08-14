d939077056fe9813169eb8f088ea4ef675e014fd
Anthony Baynham <anthonyybayn@gmail.com>
2022-08-14 23:45:14 +0100

    Much progress

---
+++ components/Banner.tsx
@@ -0,0 +1,5 @@
+function Banner({ children }) {
+  return <div className="flex flex-row gap-1 p-4 bg-purple-700 text-white">{children}</div>
+}
+
+export default Banner
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
-
-import {Navbar, NavItem} from '../components/Nav'
+import { Navbar, NavItem } from '../components/Nav'
+import Banner from '../components/Banner'
 
 function TheApp({ Component, pageProps }) {
 
   return <div className="h-[100vh]">
+    <Banner>
+        <p>This site is a work-in-progress! Check developments at</p>
+        <a href="https://github.com/anthony-y/www">https://github.com/anthony-y/www</a>
+    </Banner>
+
     <Navbar>
       <NavItem the_link="/">Showcase</NavItem>
       <NavItem the_link="/blog">Blog</NavItem>
