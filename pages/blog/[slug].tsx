2ea674f51cc7284746585d2fc403cc80d5fd1626
Anthony Baynham <anthonyybayn@gmail.com>
2022-08-16 14:43:57 +0100

    Cleanup

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,5 +1,17 @@
-function BlogPost({ props }) {
+function BlogPost({ post }) {
+  return <>
+    <div className="container mx-auto flex justify-center items-center p-20">
+      <div className="flex flex-col gap-4" dangerouslySetInnerHTML={{__html: post }}></div>
+    </div>
+  </>
+}
 
+export async function getStaticPaths() {
+  
+}
+
+export async function getStaticProps({ params }) {
+  
 }
 
 export default BlogPost
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,10 +1,18 @@
-function AllBlogPosts({ props }) {
-  return <section className="flex justify-center align-center w-full h-[100vh]">
+function AllBlogPosts({ posts }) {
+  return <section className="container mx-auto flex justify-center items-center p-12 h-[100vh]">
     <article className="my-auto flex gap-6 flex-col">
       <h1 className="text-7xl">The blog isn't ready just yet 😔</h1>
       <p>You can watch <a href="https://github.com/anthony-y/www"><span className="underline">the repository</span></a> to see when it will be, though.</p>
     </article>
   </section>
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      
+    }
+  }
 }
 
 export default AllBlogPosts
