0f8ee2e5dce752229be138f377d31a36657f7f21
Anthony Baynham <anthonyybayn@gmail.com>
2022-08-17 13:56:03 +0100

    Fixed SVG gaps

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,8 +24,8 @@ function Frontpage({ domains }: InferGetStaticPropsType<typeof getStaticProps>) 
       <DomainShowcase {...domains[0]}/>
 
       {
-      theme == 'light' && <div className={`svg-background-cover day-bottom -mt-[1px]`}></div> ||
-      theme == 'dark'  && <div className={`svg-background-cover wave-night-bottom -mt-[1px]`}></div>
+      theme == 'light' && <div className={`svg-background-cover day-bottom -mb-[1px]`}></div> ||
+      theme == 'dark'  && <div className={`svg-background-cover wave-night-bottom -mb-[1px]`}></div>
       }
 
       <DomainShowcase {...domains[1]}/>
