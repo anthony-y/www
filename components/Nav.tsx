f172a8ac306469c47aba29152fc501ffa36a62dd
anthony-y <anthonyybayn@gmail.com>
2022-09-11 11:48:17 +0100

    Fix font size of theme toggle on mobile

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
 
 export function Navbar({ children }) {
-  return <nav className="flex flex-row gap-8 md:gap-24 pt-6 justify-center dark:bg-dark">
+  return <nav className="flex flex-row gap-8 md:gap-24 pt-6 text-sm md:text-lg justify-center dark:bg-dark">
     {children}
   </nav>
 }
 
 export const NavItem = ({ children, the_link }) => {
   return <Link href={the_link}>
-    <a className="text-sm md:text-lg transition hover:text-blue-400 dark:text-white dark:hover:text-darkPrimary">
+    <a className="transition hover:text-blue-400 dark:text-white dark:hover:text-darkPrimary">
       {children}
     </a>
   </Link>
